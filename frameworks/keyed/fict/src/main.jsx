4a8d18df8d57fcffc858e513d275f448d3496a80
Michael Lin <michael.lin@ringcentral.com>
2025-12-30 02:59:19 +0800

    example-1

--- frameworks/keyed/fict/src/main.jsx
+++ frameworks/keyed/fict/src/main.jsx
@@ -28,7 +28,19 @@ const adjectives = [
   'fancy',
 ]
 
-const colors = ['red', 'yellow', 'blue', 'green', 'pink', 'brown', 'purple', 'brown', 'white', 'black', 'orange']
+const colours = [
+  'red',
+  'yellow',
+  'blue',
+  'green',
+  'pink',
+  'brown',
+  'purple',
+  'brown',
+  'white',
+  'black',
+  'orange',
+]
 
 const nouns = [
   'table',
@@ -47,84 +59,77 @@ const nouns = [
 ]
 
 let nextId = 1
-const random = max => Math.round(Math.random() * 1000) % max
+function random(max: number) {
+  return Math.round(Math.random() * 1000) % max
+}
 
-const buildData = count => {
+function buildData(count: number) {
   const data = new Array(count)
   for (let i = 0; i < count; i++) {
     data[i] = {
       id: nextId++,
-      label: `${adjectives[random(adjectives.length)]} ${colors[random(colors.length)]} ${nouns[random(nouns.length)]}`,
+      label: `${adjectives[random(adjectives.length)]} ${colours[random(colours.length)]} ${nouns[random(nouns.length)]}`,
     }
   }
   return data
 }
 
-const Button = ({ id, title, onClick }) => (
-  <div class="col-sm-6 smallpad">
-    <button type="button" class="btn btn-primary btn-block" id={id} onClick={onClick}>
-      {title}
-    </button>
-  </div>
-)
+function Button(props: any) {
+  return (
+    <div class="col-sm-6 smallpad">
+      <button id={props.id} class="btn btn-primary btn-block" type="button" onClick={props.onClick}>
+        {props.text}
+      </button>
+    </div>
+  )
+}
 
-const Row = ({ isSelected, item, select, remove }) => (
-  <tr class={isSelected ? 'danger' : ''}>
-    <td class="col-md-1">{item.id}</td>
-    <td class="col-md-4">
-      <a onClick={() => select(item.id)}>{item.label}</a>
-    </td>
-    <td class="col-md-1">
-      <a onClick={() => remove(item.id)}>
-        <span class="glyphicon glyphicon-remove" aria-hidden="true" />
-      </a>
-    </td>
-    <td class="col-md-6" />
-  </tr>
-)
-
-const Main = () => {
-  let items = $state([])
-  let selected = $state(0)
+function App() {
+  let data: { id: number; label: string }[] = $state([])
+  let selected: number | null = $state(null)
 
   const run = () => {
-    items = buildData(1_000)
-    selected = 0
+    data = buildData(1000)
+    selected = null
   }
 
   const runLots = () => {
-    items = buildData(10_000)
-    selected = 0
+    data = buildData(10000)
+    selected = null
   }
 
   const add = () => {
-    items = items.concat(buildData(1_000))
-  }
-
-  const clear = () => {
-    items = []
-    selected = 0
+    data = [...data, ...buildData(1000)]
   }
 
   const update = () => {
-    const updated = items.slice()
-    for (let i = 0; i < updated.length; i += 10) {
-      const r = updated[i]
-      updated[i] = { id: r.id, label: `${r.label} !!!` }
-    }
-    items = updated
+    data = data.map((row, i) => (i % 10 === 0 ? { ...row, label: row.label + ' !!!' } : row))
   }
 
   const swapRows = () => {
-    if (items.length < 999) return
-    const swapped = items.slice()
-    ;[swapped[1], swapped[998]] = [swapped[998], swapped[1]]
-    items = swapped
+    const list = data
+    if (list.length <= 998) return
+    const copy = list.slice()
+    const tmp = copy[1]
+    copy[1] = copy[998]
+    copy[998] = tmp
+    data = copy
   }
 
-  const remove = id => {
-    items = items.filter(item => item.id !== id)
-    if (selected === id) selected = 0
+  const clear = () => {
+    data = []
+    selected = null
+  }
+
+  const remove = (id: number) => {
+    data = data.filter(row => row.id !== id)
+    if (selected === id) {
+      selected = null
+    }
+  }
+
+  const select = (id: number) => {
+    selected = id
   }
 
   return (
@@ -132,30 +137,41 @@ const Main = () => {
       <div class="jumbotron">
         <div class="row">
           <div class="col-md-6">
-            <h1>Fict (keyed)</h1>
+            <h1>Fict Keyed</h1>
           </div>
           <div class="col-md-6">
             <div class="row">
-              <Button id="run" title="Create 1,000 rows" onClick={run} />
-              <Button id="runlots" title="Create 10,000 rows" onClick={runLots} />
-              <Button id="add" title="Append 1,000 rows" onClick={add} />
-              <Button id="update" title="Update every 10th row" onClick={update} />
-              <Button id="clear" title="Clear" onClick={clear} />
-              <Button id="swaprows" title="Swap Rows" onClick={swapRows} />
+              <Button id="run" text="Create 1,000 rows" onClick={run} />
+              <Button id="runlots" text="Create 10,000 rows" onClick={runLots} />
+              <Button id="add" text="Append 1,000 rows" onClick={add} />
+              <Button id="update" text="Update every 10th row" onClick={update} />
+              <Button id="clear" text="Clear" onClick={clear} />
+              <Button id="swaprows" text="Swap Rows" onClick={swapRows} />
             </div>
           </div>
         </div>
       </div>
       <table class="table table-hover table-striped test-data">
         <tbody>
-          {items.map(item => (
-            <Row key={item.id} item={item} isSelected={selected === item.id} select={id => (selected = id)} remove={remove} />
+          {data.map(row => (
+            <tr key={row.id} class={selected === row.id ? 'danger' : ''}>
+              <td class="col-md-1">{row.id}</td>
+              <td class="col-md-4">
+                <a onClick={() => select(row.id)}>{row.label}</a>
+              </td>
+              <td class="col-md-1">
+                <a onClick={() => remove(row.id)}>
+                  <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                </a>
+              </td>
+              <td class="col-md-6"></td>
+            </tr>
           ))}
         </tbody>
       </table>
-      <span class="preloadicon glyphicon glyphicon-remove" aria-hidden="true" />
+      <span class="preloadicon glyphicon glyphicon-remove" aria-hidden="true"></span>
     </div>
   )
 }
 
-render(() => <Main />, document.getElementById('main'))
+render(() => <App />, document.getElementById('main'))
